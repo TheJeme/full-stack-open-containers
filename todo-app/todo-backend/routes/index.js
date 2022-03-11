6af010d73222c6198b2c011a014c71c93e484305
TheJeme <eemelijoonatan@gmail.com>
2022-03-11 10:00:53 +0200

    done

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require("../util/config");
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+router.get("/statistics", async (req, res) => {
+  const todos = await getAsync("added_todos");
+
+  res.json({ todos });
 });
 
 module.exports = router;
